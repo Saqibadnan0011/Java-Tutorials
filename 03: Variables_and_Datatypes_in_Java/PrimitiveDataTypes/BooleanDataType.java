c4c51008e91ea23aba6549105ec0b60e55846ef6
Saqib Adnan <Saqibadnan7755@gmail.com>
2024-03-23 20:20:00 +0500

    Boolean code example

---
+++ 03: Variables_and_Datatypes_in_Java/PrimitiveDataTypes/BooleanDataType.java
@@ -0,0 +1,16 @@
+public class BooleanDataType {
+    public static void main(String[] args) {
+        int n1 = 25;
+        int n2 = 18;
+
+        // declaring true or false
+        boolean b1 = true;
+        boolean b2 = false;
+
+        if (n1 > n2) {
+            System.out.println(b1);
+        } else {
+            System.out.println(b2);
+        }
+    }
+}
