c0da4e0bf98b73e9b3f6293142f5d4bbfe31e5fc
Saqib Adnan <Saqibadnan7755@gmail.com>
2024-03-23 19:50:26 +0500

    Primitive Datatype

---
+++ 03: Variables_and_Datatypes_in_Java/PrimitiveDataTypes/DataTypes.java
@@ -0,0 +1,24 @@
+public class DataTypes {
+    public static void main(String[] args) {
+        // Primitive Datatype
+
+        // Boolean
+        boolean result = true;
+        // Character
+        char capitalC = 'C';
+        // Byte
+        byte b = 100;
+        // Short
+        short s = 10000;
+        // Integer
+        int i = 1000000;
+        // Double
+        double d2 = 1.234e2;
+        // Double
+        double d1 = 123.4;
+        // Float
+        float f1 = 123.4f;
+        // Long
+        long l = 122343334444L;
+    }
+}
